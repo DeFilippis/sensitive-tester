147b0b7e36be65d619103084ca41e683073f49e5
chapkovski <chapkovski@gmail.com>
2020-10-21 10:16:22 +0300

    moving

--- tester/models.py
+++ tester/models.py
@@ -8,12 +8,12 @@ from otree.api import (
     Currency as c,
     currency_range,
 )
+from django.db import models as djmodels
 
-
-author = 'Your name here'
+author = 'Chapkovski, De Filippis, Henig-Schmidt'
 
 doc = """
-Your app description
+App testing for the most controversial questions for the conformity project
 """
 
 
@@ -21,6 +21,7 @@ class Constants(BaseConstants):
     name_in_url = 'tester'
     players_per_group = None
     num_rounds = 1
+    LIKERT = range(0, 11)
 
 
 class Subsession(BaseSubsession):
@@ -33,3 +34,25 @@ class Group(BaseGroup):
 
 class Player(BasePlayer):
     pass
+
+class ProtoQ(djmodels.Model):
+    body = models.StringField()
+
+class SensitiveQ(djmodels.Model):
+    owner = djmodels.ForeignKey(to=Player, on_delete=djmodels.CASCADE, related_name="sqs")
+    prototype = djmodels.ForeignKey(to=ProtoQ, on_delete=djmodels.CASCADE, related_name="sqs")
+    attitude = models.IntegerField(choices=Constants.LIKERT, widget=widgets.RadioSelectHorizontal)
+    average_attitude = models.IntegerField(choices=Constants.LIKERT, widget=widgets.RadioSelectHorizontal)
+    """
+    The block of the following questions is to elicit info about distribution shape.
+    We may think about something more complex later on. 
+    """
+    first = models.IntegerField(min=0, max=100)
+    second = models.IntegerField(min=0, max=100)
+    third = models.IntegerField(min=0, max=100)
+from django.db.models.signals import post_migrate,class_prepared
+def my_callback(sender, **kwargs):
+    print('migration complete')
+    pass
+post_migrate.connect(my_callback)
+class_prepared.connect(my_callback)
