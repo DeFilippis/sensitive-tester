85246d3309933404330615dca3941f7a2dac9bb5
DeFilippis <edefilippis@flourish.local>
2020-11-15 23:53:50 -0500

    added religion question, changed background to dark grey, added morally neutral question

---
+++ .history/sensitive_tester_endline/pages_20201115233342.py
@@ -0,0 +1,24 @@
+from otree.api import Currency as c, currency_range
+
+from ._builtin import Page, WaitPage
+from .models import Constants
+
+import re
+import csv
+import random
+
+
+class Endline(Page):
+    form_model = 'player'
+    form_fields = ['sex', 'birth', 'race', 'education', 'income', 'religion', 'religious_attendance', 'feedback']
+
+page_sequence = [
+        Endline,
+]
+
+
+
+
+                   
+            
+            
